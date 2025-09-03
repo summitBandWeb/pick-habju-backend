aa4e3dc4ddee2b5b8aa63af6f9c93033eb886b4a
szoon2426 <s.zoon2426@gmail.com>
2025-09-04 01:57:51 +0900

    [#62]; fix: vercel용 cors 추가

--- app/main.py
+++ app/main.py
@@ -26,6 +26,7 @@ origins = list({
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_origin_regex=r"^https:\/\/pick\-habju\-frontend.*\.vercel\.app$",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
