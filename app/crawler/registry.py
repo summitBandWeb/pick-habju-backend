d2c3528e93767c295abbc4788918e3b0346d1afd
jhHa0320 <snake6862@gmail.com>
2026-01-06 12:53:31 +0900

    [#83]; refactor: 합주실 크롤러 실행 코드의 하드코딩 부분 개선

---
+++ app/crawler/registry.py
@@ -0,0 +1,12 @@
+from app.crawler.dream_checker import get_dream_availability
+from app.crawler.groove_checker import get_groove_availability
+from app.crawler.naver_checker import get_naver_availability
+
+CRAWLER_REGISTRY = {
+    # "합주실 타입": 크롤러함수
+    "dream": get_dream_availability,
+    "groove": get_groove_availability,
+    "naver": get_naver_availability, 
+    # "hapjusil": get_hapjusil_availability,
+    # "kakao": get_kakao_availaiblity
+}
