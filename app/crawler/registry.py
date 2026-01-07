0033ea2491b0d8758ea6fca49d3da426e7db9f41
jhHa0320 <snake6862@gmail.com>
2026-01-07 23:46:26 +0900

    [#83]; refactor: 타입 어노테이션 추가 및 타입 안정성 개선

--- app/crawler/registry.py
+++ app/crawler/registry.py
@@ -1,8 +1,13 @@
+from typing import Callable, List, Awaitable, Union
+from app.models.dto import RoomKey, RoomAvailability
+from app.utils.room_router import RoomType
 from app.crawler.dream_checker import get_dream_availability
 from app.crawler.groove_checker import get_groove_availability
 from app.crawler.naver_checker import get_naver_availability
 
-CRAWLER_REGISTRY = {
+RoomResult = Union[RoomAvailability, Exception]
+
+CRAWLER_REGISTRY: dict[RoomType, Callable[[str, List[str], List[RoomKey]], Awaitable[List[RoomResult]]]] = {
     # "합주실 타입": 크롤러함수
     "dream": get_dream_availability,
     "groove": get_groove_availability,
